2c1dd15ad07a370ee58e31e3117e791e5916f5c9
zkatz100 <115914189+zkatz100@users.noreply.github.com>
2026-08-20 18:16:43 -0400

    Update queries.sql

--- hive/queries.sql
+++ hive/queries.sql
@@ -1,9 +1,5 @@
--- DSC 650 Portfolio Starter
--- Replace these comments with representative Hive queries from your final project.
---
--- Choose queries that demonstrate meaningful work rather than every query you ran.
+-- In my first querie, I listed the first ten rows of the HIVE table to show that the data loaded corectly.
+SELECT * FROM performance LIMIT 10;
 
--- Example:
--- SELECT ...
--- FROM ...
--- WHERE ...;
+-- In my second querie, I counted the number of students in the table that had earned an 'f' as their overall final grade.
+SELECT COUNT(*) FROM performance WHERE final_grade = 'f';
