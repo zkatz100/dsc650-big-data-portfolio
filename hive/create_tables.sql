faefa356ce60ebf0a1014f66c6ecead84a6fb229
zkatz100 <115914189+zkatz100@users.noreply.github.com>
2026-08-20 17:27:59 -0400

    Update create_tables.sql

--- hive/create_tables.sql
+++ hive/create_tables.sql
@@ -1,14 +1,21 @@
--- DSC 650 Portfolio Starter
--- Replace this file with the Hive DDL from your final project.
---
--- Before publishing:
---   1. Remove credentials or environment-specific secrets.
---   2. Add short comments explaining important tables.
---   3. Keep the SQL that best demonstrates your work.
-
--- Example structure only:
--- CREATE EXTERNAL TABLE your_table (
---     id STRING,
---     ...
--- )
--- STORED AS ...;
+CREATE TABLE performance (
+`student_id` INT,
+`age` INT,
+`gender` STRING,
+`school_type` STRING,
+`parent_education` STRING,
+`study_hours` STRING,
+`attendance_percentage` STRING,
+`internet_access` STRING,
+`travel_time` STRING,
+`extra_activities` STRING,
+`study_method` STRING,
+`math_score` STRING, 
+`science_score` STRING, 
+`english_score` STRING,
+`overall_score` STRING,
+`final_grade` STRING)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+tblproperties("skip.header.line.count"="1");
